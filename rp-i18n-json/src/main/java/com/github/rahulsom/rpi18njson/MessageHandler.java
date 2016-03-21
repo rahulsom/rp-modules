2faead542399985eb411469a2f0b3cf212d10360
Rahul Somasunderam <rahul.som@gmail.com>
2016-03-21 15:34:50 -0700

    Add support for i18n

---
+++ rp-i18n-json/src/main/java/com/github/rahulsom/rpi18njson/MessageHandler.java
@@ -0,0 +1,53 @@
+package com.github.rahulsom.rpi18njson;
+
+import com.google.inject.Inject;
+import ratpack.handling.Context;
+import ratpack.handling.Handler;
+import ratpack.jackson.Jackson;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * Created by rahul on 3/21/16.
+ */
+public class MessageHandler implements Handler {
+
+    @Inject
+    MessageModule.Config config;
+
+    @Override
+    public void handle(Context ctx) throws Exception {
+        String acceptLanguageHeader = ctx.getRequest().getHeaders().get("Accept-Language");
+        Map<String, String> retval = getBundleAsMap(null);
+        Locale.LanguageRange.parse(acceptLanguageHeader).
+                stream().
+                sorted((o1, o2) -> o1.getWeight() - o2.getWeight() > 0 ? 1 : -1).
+                map(Locale.LanguageRange::getRange).
+                forEach(s -> retval.putAll(getBundleAsMap(s)));
+        ctx.render(Jackson.json(retval));
+    }
+
+    Map<String, String> getBundleAsMap(String locale) {
+        Properties prop = new Properties();
+        String resourceName = locale == null ? config.getBundleName() : (config.getBundleName() + "_" + locale);
+        InputStream ir = this.getClass().getClassLoader().getResourceAsStream(resourceName + ".properties");
+        if (ir != null) {
+            try {
+                prop.load(ir);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Map<String, String> retval = new HashMap<>();
+        for (String k : prop.stringPropertyNames()) {
+            retval.put(k, (String) prop.get(k));
+        }
+        return retval;
+    }
+
+}
