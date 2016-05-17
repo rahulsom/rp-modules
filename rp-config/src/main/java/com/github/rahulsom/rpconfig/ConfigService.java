e1bf0d07af4a4233cc1c28918dbeaaf324eed3f9
Rahul Somasunderam <rsom@certifydatasystems.com>
2016-05-17 10:40:21 -0700

    Better logging in ConfigService

--- rp-config/src/main/java/com/github/rahulsom/rpconfig/ConfigService.java
+++ rp-config/src/main/java/com/github/rahulsom/rpconfig/ConfigService.java
@@ -1,18 +1,22 @@
 package com.github.rahulsom.rpconfig;
 
-import com.google.common.base.MoreObjects;
 import com.google.common.io.CharStreams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.introspector.Property;
+import org.yaml.snakeyaml.representer.Representer;
 import ratpack.registry.Registry;
 import ratpack.server.ServerConfig;
 import ratpack.server.Service;
 import ratpack.server.StartEvent;
 
+import java.beans.IntrospectionException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Set;
 
 class ConfigService<T> implements Service {
 
@@ -25,11 +29,10 @@ class ConfigService<T> implements Service {
     private final Logger log = LoggerFactory.getLogger(ConfigService.class);
 
     public void onStart(StartEvent event) throws Exception {
-        log.info("OnStart info");
         final Registry registry = event.getRegistry();
         ServerConfig sc = registry.get(ServerConfig.class);
         if (sc.isDevelopment()) {
-            log.info("Running in dev mode");
+            log.debug("Running in dev mode. No banner will be printed.");
         } else {
             try {
                 final InputStream bannerStream = this.getClass().getClassLoader().getResourceAsStream("banner.txt");
@@ -40,8 +43,19 @@ class ConfigService<T> implements Service {
             }
         }
 
-        final Yaml yaml = new Yaml();
-        log.error("Events: {}", yaml.dump(config));
+        DumperOptions dumperOptions = new DumperOptions();
+        dumperOptions.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+        GroovyAwareRepresenter representer = new GroovyAwareRepresenter();
+        final Yaml yaml = new Yaml(representer, dumperOptions);
+        log.info("Configuration is\n{}", yaml.dump(config));
     }
 
+    private static class GroovyAwareRepresenter extends Representer {
+        @Override
+        protected Set<Property> getProperties(Class<? extends Object> type) throws IntrospectionException {
+            Set<Property> properties = super.getProperties(type);
+            properties.removeIf(property -> property.getName().equals("metaClass"));
+            return properties;
+        }
+    }
 }
